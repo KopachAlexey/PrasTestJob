bbb5149545d24f7c07ab8efe3f9f3e2fac444e17
Alexey_Kopach <alekseykopach777@gmail.com>
2025-11-12 16:15:44 +0300

    Api контроллеры

---
+++ PrasTestJobWeb/ApiControllers/NewsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PrasTestJobWeb.ApiControllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NewsController : ControllerBase
+    {
+    }
+}
