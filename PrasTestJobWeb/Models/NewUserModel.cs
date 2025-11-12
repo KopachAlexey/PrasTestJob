cf42025405ef5e9b432b5a3b92d717e9bd717a68
Alexey_Kopach <alekseykopach777@gmail.com>
2025-11-12 16:16:12 +0300

    Модели представления

---
+++ PrasTestJobWeb/Models/NewUserModel.cs
@@ -0,0 +1,9 @@
+﻿namespace PrasTestJobWeb.Models
+{
+    public class NewUserModel
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string RoleName { get; set; }
+    }
+}
---
+++ PrasTestJobWeb/Models/UserLoginModel.cs
@@ -0,0 +1,9 @@
+﻿namespace PrasTestJobWeb.Models
+{
+    public class UserLoginModel
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+}
