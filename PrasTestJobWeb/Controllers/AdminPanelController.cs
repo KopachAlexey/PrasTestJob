62d4952b3fe05a81ac0b012c23340d44328bd2b6
Alexey_Kopach <alekseykopach777@gmail.com>
2025-11-12 16:15:57 +0300

    Mvc контроллеры

---
+++ PrasTestJobWeb/Controllers/AdminPanelController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PrasTestJobDTO;
+using PrasTestJobServices.Abstract;
+using PrasTestJobWeb.Models;
+
+namespace PrasTestJobWeb.Controllers
+{
+    public class AdminPanelController : Controller
+    {
+        readonly IUserServices _userServices;
+        readonly IPasswordHashing _passwordHashing;
+        readonly INewsServices _newsServices;
+
+        public AdminPanelController(IUserServices userServices, IPasswordHashing passwordHashing, INewsServices newsServices)
+        {
+            _userServices = userServices;
+            _passwordHashing = passwordHashing;
+            _newsServices = newsServices;
+        }
+
+        public IActionResult AdminPanel()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> AddUserProcessing([FromForm] NewUserModel newUser)
+        {
+            try
+            {
+                var user = await _userServices.GetUserByLoginAsync(newUser.Login);
+                if (user is not null)
+                    return RedirectToAction(nameof(Index));
+                var createUserDto = new CreateUserDto
+                {
+                    Login = newUser.Login,
+                    PasswordHas = _passwordHashing.HashPassword(newUser.Password),
+                    RoleName = newUser.RoleName
+                };
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ PrasTestJobWeb/Controllers/AuthenticationController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using PrasTestJobServices.Abstract;
+using PrasTestJobWeb.Models;
+using PrasTestJobDTO;
+using System.Security.Claims;
+
+namespace PrasTestJobWeb.Controllers
+{
+    public class AuthenticationController : Controller
+    {
+        const string _mainPageUrl = "";
+
+        readonly IUserServices _userServices;
+        readonly IPasswordHashing _passwordHashing;
+
+        public AuthenticationController(IUserServices userServices, IPasswordHashing passwordHashing)
+        {
+            _userServices = userServices;
+            _passwordHashing = passwordHashing;
+        }
+
+        [HttpGet]
+        public IActionResult Login(string? returnUrl = null, bool? loginResult = null)
+        {
+            ViewBag.ReturnUrl = returnUrl ?? _mainPageUrl;
+            ViewBag.LoginResult = loginResult;
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> LoginProcessing([FromForm] UserLoginModel loginModel)
+        {
+            UserDto? user;
+            try
+            {
+                user = await _userServices.GetUserByLoginAsync(loginModel.Login);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            if (user is null || !_passwordHashing.VerifyPassword(loginModel.Password, user.PasswordHas))
+                return RedirectToAction(nameof(Login), new { loginModel.Login, LoginResult = false});
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.Login),
+                new Claim(ClaimTypes.Role, user.RoleName),
+            };
+            var authProperties = new AuthenticationProperties
+            {
+                IsPersistent = false,
+                ExpiresUtc = DateTime.UtcNow.AddMonths(1)
+            };
+            var identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+            var principal = new ClaimsPrincipal(identity);
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal, authProperties);
+            if (!String.IsNullOrEmpty(loginModel.ReturnUrl) && Url.IsLocalUrl(loginModel.ReturnUrl))
+                return LocalRedirect(loginModel.ReturnUrl);
+            else
+                return Redirect(_mainPageUrl);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return Redirect(_mainPageUrl);
+        }
+
+        [HttpGet]
+        public IActionResult AccessDenied()
+        {
+            return View();
+        }
+
+    }
+}
