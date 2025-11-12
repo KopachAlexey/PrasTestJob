7b7e6faafa5d87fcdfae2cd47e09092b13560ac9
Alexey_Kopach <alekseykopach777@gmail.com>
2025-11-12 16:09:07 +0300

    Контекст базы данных PrasTestJob

---
+++ PrasTestJobData/PrasTestJobContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PrasTestJobData.Entities;
+using PrasTestJobData.EntitiesConfigration;
+
+namespace PrasTestJobData
+{
+    public class PrasTestJobContext : DbContext
+    {
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<News> News { get; set; }
+
+        public PrasTestJobContext(DbContextOptions<PrasTestJobContext> options) :base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new UserConfiguration());
+            modelBuilder.ApplyConfiguration(new RoleConfiguration());
+            modelBuilder.ApplyConfiguration(new NewsConfiguration());
+        }
+    }
+}
