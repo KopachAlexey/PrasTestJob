6e9c93e54c5612a51983f1bbda3308c8cfd4eef9
Alexey_Kopach <alekseykopach777@gmail.com>
2025-11-14 13:18:15 +0300

    В базу данных добавленны данные для таблицы Role.

--- PrasTestJobData/Migrations/20251114083928_AddRoleData.Designer.cs
+++ PrasTestJobData/Migrations/20251114083928_AddRoleData.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PrasTestJobData;
 
@@ -10,9 +11,11 @@ using PrasTestJobData;
 namespace PrasTestJobData.Migrations
 {
     [DbContext(typeof(PrasTestJobContext))]
-    partial class PrasTestJobContextModelSnapshot : ModelSnapshot
+    [Migration("20251114083928_AddRoleData")]
+    partial class AddRoleData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.10");
